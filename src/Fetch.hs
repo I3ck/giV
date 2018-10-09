200e3d366e08d73d72ac995250e2acbbb7d895e6
Martin Buck <buckmartin@buckmartin.de>
2018-10-09 20:17:29 +0200

    simplified Fetch

--- src/Fetch.hs
+++ src/Fetch.hs
@@ -8,18 +8,4 @@ import           System.Process
 --------------------------------------------------------------------------------
 
 fetchCommitString :: IO CommitString
-fetchCommitString = gcommits
-
---------------------------------------------------------------------------------
-
-git :: Params -> IO String
-git ps = readProcess "git" ps ""
-
-glog :: Params -> IO String
-glog ps = git ("log" : ps)
-
-glogfull :: Params -> IO String
-glogfull ps = glog ("--full-history" : ps)
-
-gcommits :: IO CommitString
-gcommits = CommitString <$> glogfull ["--pretty=format:%d|%s"]
+fetchCommitString = CommitString <$> readProcess "git" ["log", "--pretty=format:%d|%s", "--full-history"] ""
