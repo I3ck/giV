e4511bde0b50e17843382d0cde11c6e0bffcbcaf
Martin Buck <buckmartin@buckmartin.de>
2018-10-14 19:40:42 +0200

    cleanup of fetch

--- src/IO/Fetch.hs
+++ src/IO/Fetch.hs
@@ -7,15 +7,19 @@ import           Types
 
 --------------------------------------------------------------------------------
 
----TODO some args are used 3 times here, avoid duplication
-
 fetchCommitString :: Branch -> IO (BranchMaster CommitString)
+
 fetchCommitString (Branch "master") = do
-  result <- readProcess "git" ["log", "--reverse", "--pretty=format:%d|%s", "--first-parent", "master", "--"] ""
+  result <- readProcess "git" (["log", "master"] ++ sharedArgs) ""
   pure $ BranchMaster{bMaster = CommitString result, bBranch = CommitString []}
 
 fetchCommitString (Branch br) = do --TODO try and avoid duplicate call here (git command which does all at once?)
-  resultOnlyBranch <- readProcess "git" ["log", "master.." ++ br, "--reverse", "--pretty=format:%d|%s", "--first-parent"] ""
-  resultTotal      <- readProcess "git" ["log", "--reverse", "--pretty=format:%d|%s", "--first-parent", br, "--"] ""
+  resultOnlyBranch <- readProcess "git" (["log", "master.." ++ br] ++ sharedArgs) ""
+  resultTotal      <- readProcess "git" (["log", br] ++ sharedArgs) ""
   let resultMaster = take (length resultTotal - length resultOnlyBranch) resultTotal
   pure $ BranchMaster{bMaster = CommitString resultMaster, bBranch = CommitString resultOnlyBranch}
+
+--------------------------------------------------------------------------------
+
+sharedArgs:: [String]
+sharedArgs= ["--reverse", "--pretty=format:%d|%s", "--first-parent", "--"]
